9299e3c74cfdbf5679415afaeb7f426544bb6f75
JonathWesley <jonathherdt@gmail.com>
2019-09-26 18:44:17 -0300

    comecando a arrumar o web

--- codigo/dispositivo/dispositivo.ino
+++ codigo/dispositivo/dispositivo.ino
@@ -8,25 +8,32 @@
 #define MQ 2
 #define pinoDHT11 4
 #define BTN 5
-#define LED  18
+#define LedVermelho  18
+#define LedVerde  19
 #define BUZZER 21
 
 #define HOST "controle-de-incendio.firebaseio.com"
 #define DBKEY "aLFh7i8DxeQTiSBLyoFPKF1OjUIK3xETGZPVttUw"
 
-#define WF_SSID "CORNAO"
-#define WF_PASSWORD "corno123"
+#define WF_SSID "jonath-Inspiron-5558"
+#define WF_PASSWORD "Be2ftpT0"
 
 FirebaseData firebaseData;
 
 dht DHT;
 
-int gas_threshold = 800;
+/*Configuracoes do dispositivo*/
+int id_dispositivo = 1;
+
+/*Variaveis para ativacao do alerta*/
+int gas_threshold = 2000;
 int temp_threshold = 20;
-int humi_threshold = 10;
+int humi_threshold = 80;
+
+/*Variaveis para controle do codigo*/
 int mq_input = 0;
-int btn = 0;
-int cont = 0;
+bool salvo = false;
+char dispositivoStatus = '1'; // 1->ligado ; 2->acionado ; 0->desligado
 int buzzer = 0;
 
 void setup() {
@@ -37,35 +44,101 @@ void setup() {
     delay(500);
     Serial.println("Connecting to WiFi..");
   }
+  Serial.println("WiFi connected.");
   /*******************************/
   Firebase.begin(HOST, DBKEY);
   Firebase.reconnectWiFi(true);
   /********************************/
 
-  pinMode(BTN, INPUT);
-  pinMode(LED, OUTPUT);
+  pinMode(LedVermelho, OUTPUT);
+  pinMode(LedVerde, OUTPUT);
   pinMode(BUZZER, OUTPUT);
   pinMode(MQ, INPUT);
+  pinMode(BTN, INPUT);
+
+  digitalWrite(LedVermelho, LOW);
+  digitalWrite(LedVerde, HIGH);
+  digitalWrite(BUZZER, LOW);
 
   Serial.begin(115200);
+
+  Serial.println("DISPOSITIVO LIGADO!!!");
 }
 
 void loop() {
-  mq_input = analogRead(MQ);
-  DHT.read11(pinoDHT11);
+  if(dispositivoStatus == '1'){
+    mq_input = analogRead(MQ);
+    DHT.read11(pinoDHT11);
   
-  Serial.print("Gas: ");
-  Serial.println(mq_input);
-  Serial.print("Temperatura: ");
-  Serial.print(DHT.temperature, 0);
-  Serial.println("*C");
-  Serial.print("Umidade: ");
-  Serial.print(DHT.humidity, 0);
-  Serial.println("%");
-  Serial.print("BOTAO: ");
-  Serial.println(digitalRead(BTN));
+    /******************************/
+    Serial.print("Gas: ");
+    Serial.println(mq_input);
+    Serial.print("Temperatura: ");
+    Serial.print(DHT.temperature, 0);
+    Serial.println("*C");
+    Serial.print("Umidade: ");
+    Serial.print(DHT.humidity, 0);
+    Serial.println("%");
+    /******************************/
   
-  if (mq_input > gas_threshold && DHT.temperature > temp_threshold && DHT.humidity > humi_threshold){
+    if(DHT.temperature > (temp_threshold*1.5)){
+      do{
+        salvo = enviarDados(mq_input, DHT.temperature, DHT.humidity);
+      }while(!salvo);
+      
+      ativaAlarme();
+    }else if(mq_input > (gas_threshold*1.5) && DHT.temperature > (temp_threshold*0.5)){
+      do{
+        salvo = enviarDados(mq_input, DHT.temperature, DHT.humidity);
+      }while(!salvo);
+      
+      ativaAlarme();
+    }else if (mq_input > gas_threshold && DHT.temperature > temp_threshold){
+      do{
+        salvo = enviarDados(mq_input, DHT.temperature, DHT.humidity);
+      }while(!salvo);
+      
+      ativaAlarme();
+    }
+
+    /*Desligar Dispositivo*/
+    if(digitalRead(BTN) == HIGH){
+      dispositivoStatus = '0';
+      digitalWrite(LedVerde, LOW);
+    }
+  }else{
+    Serial.println("DISPOSITIVO DESLIGADO!!!");
+    
+    /*Ligar Dispositivo*/
+    if(digitalRead(BTN) == HIGH){
+      dispositivoStatus = '1';
+      digitalWrite(LedVerde, HIGH);
+    }
+  }
+  delay(200);
+}
+
+bool enviarDados(int fumaca, int temperatura, int umidade){
+  FirebaseJson ocorrencia;  
+  ocorrencia.addInt("id_dispositivo", id_dispositivo).addInt("temperatura", temperatura).addInt("desidade_fumaca", fumaca).addInt("umidade", umidade);
+      
+  if(Firebase.pushJSON(firebaseData, "/ocorrencias", ocorrencia)){
+    Serial.println(firebaseData.dataPath());
+    Serial.println(firebaseData.pushName());
+    return true;
+  }else{
+    Serial.println(firebaseData.errorReason());
+    return false;
+  }
+}
+
+void ativaAlarme(){
+  digitalWrite(LedVerde, LOW);
+  dispositivoStatus = '2';
+
+  bool sair = true;
+  while(sair){
+    /*Alarmes Locais*/
     if(buzzer == 0){
       digitalWrite(BUZZER, HIGH);
       buzzer = 1;
@@ -73,34 +146,15 @@ void loop() {
       digitalWrite(BUZZER, LOW);
       buzzer = 0;
     }
+    digitalWrite(LedVermelho, HIGH);
     
-    digitalWrite(LED, HIGH);
-    if(cont == 0){
-      FirebaseJson ocorrencia;
-      
-      ocorrencia.addString("local", "UNIVALI").addInt("temperatura", DHT.temperature);
-      
-      if(Firebase.pushJSON(firebaseData, "/ocorrencias", ocorrencia)){
-        Serial.println(firebaseData.dataPath());
-        Serial.println(firebaseData.pushName());
-        Serial.println(firebaseData.dataPath() + "/"+ firebaseData.pushName());
-        cont += 1;
-      } else {
-        Serial.println(firebaseData.errorReason());
-      }
-    }
+    /*Desativa alarme*/
     if(digitalRead(BTN) == HIGH){
-        digitalWrite(BUZZER, LOW);
-        digitalWrite(LED, LOW);
-        cont = 0;
-        delay(10000);
-      }
-    //digitalWrite(BUZZER, HIGH); 
-    Serial.println("TA PEGANDO FOGO BIXO!!!!");
-  }else{
-    //digitalWrite(BUZZER, LOW);
-    Serial.println("Ta suave...");
+      digitalWrite(BUZZER, LOW);
+      digitalWrite(LedVermelho, LOW);
+      dispositivoStatus = '0';
+      sair = false;
+      delay(2000);
+    }
   }
-  
-  delay(100);
-} 
+}
